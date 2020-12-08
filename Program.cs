a06094b89a0d3cb6f66cc9833d6b8cab3e80e8ca
Dan Corder <dev@dancorder.com>
2020-12-08 07:02:20 +0000

    Day 8

--- Program.cs
+++ Program.cs
@@ -20,9 +20,9 @@ namespace advent_of_code_2020
             // Console.WriteLine(Day06.SolveProblem1());
             // Console.WriteLine(Day06.SolveProblem2());
             // Console.WriteLine(Day07.SolveProblem1());
-            Console.WriteLine(Day07.SolveProblem2());
+            // Console.WriteLine(Day07.SolveProblem2());
             // Console.WriteLine(Day08.SolveProblem1());
-            // Console.WriteLine(Day08.SolveProblem2());
+            Console.WriteLine(Day08.SolveProblem2());
             // Console.WriteLine(Day09.SolveProblem1());
             // Console.WriteLine(Day09.SolveProblem2());
             // Console.WriteLine(Day10.SolveProblem1());
