1ba372ee7fbba4d227d224447d6af81abc9277ed
Dan Corder <dev@dancorder.com>
2020-12-17 06:55:08 +0000

    Day 16

--- Program.cs
+++ Program.cs
@@ -36,9 +36,9 @@ namespace advent_of_code_2020
             // Console.WriteLine(Day14.SolveProblem1());
             // Console.WriteLine(Day14.SolveProblem2());
             // Console.WriteLine(Day15.SolveProblem1());
-            Console.WriteLine(Day15.SolveProblem2());
+            // Console.WriteLine(Day15.SolveProblem2());
             // Console.WriteLine(Day16.SolveProblem1());
-            // Console.WriteLine(Day16.SolveProblem2());
+            Console.WriteLine(Day16.SolveProblem2());
             // Console.WriteLine(Day17.SolveProblem1());
             // Console.WriteLine(Day17.SolveProblem2());
             // Console.WriteLine(Day18.SolveProblem1());
